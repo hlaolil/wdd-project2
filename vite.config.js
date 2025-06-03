d33d4fbb2db3457e855bf83b31f3d95d04961bd6
hlaolil <167699704+hlaolil@users.noreply.github.com>
2025-06-03 21:06:50 +0200

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+  publicDir: "./public",
+  build: {
+    outDir: "../dist",    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        checkoutSuccess: resolve(__dirname, "src/checkout/success.html"),
+        productDetail: resolve(__dirname, "src/product_pages/index.html"),
+        productListing: resolve(__dirname, "src/product-listing/index.html"),
+      },
+    },// Ensure static assets are copied to dist
+    assetsInclude: ["**/*.json", "**/*.svg"],
+    copyPublicDir: true,
+  },
+});
